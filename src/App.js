d86ef206916cab3710d9ed101c30ab10f3e4c003
Dushyant Jangid <dushyantjangid1122@gmail.com>
2023-03-22 19:04:04 +0530

    $th project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React ,{Component, component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello from react!!!!</h1>
+//       <h1>Hello from react!!!!</h1>
+//       <Person/>
+//     </div>
+//   );
+// }
+class App extends Component{
+  render()
+ { return(
+    <div className='App'>
+      <h1> Hello, React App</h1>
+      <p> This is Working!!</p>
+      <Person/>
+      <Person/>
+      <Person/>
     </div>
-  );
+  )}
 }
+
+
 
 export default App;
